6cdaef7808d37773d7e6bcf4c3d4b6e3e322eb59
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-06-03 12:38:39 +0300

    робота на уроці

--- js/01-object-prototype.js
+++ js/01-object-prototype.js
@@ -8,6 +8,28 @@
  * - Власні властивості і Object.prototype.hasOwnProperty()
  * - Цепочка прототипів
  */
+// const objC = {
+//   z: 5,
+// };
+
+// const objB = Object.create(objC);
+// objB.y = 2;
+
+// console.log('objB', objB);
+// console.log('objB.y', objB.y);
+// console.log('objB.z', objB.z);
+// // clo - консолька для того щоб підписати
+
+// console.log('objC', objC);
+
+// const objA = Object.create(objB);
+// objA.x = 1;
+
+// console.log('objA', objA);
+// objA.z = 1000;
+// console.log('objA.z', objA.z);
+// console.log(objC.hasOwnProperty('x'));
+// console.log(objC.hello);
 
 /*
  * Алгоритм пошуку властивостей в цепочке прототипів:
@@ -16,3 +38,10 @@
  * 3. Пошук припиняється при першому співпадінню (є така властивість)
  * 4. Повертається значення властивості
  */
+
+const dummyObj = Object.create({ message: 'Це властивість обєкта прототипа' });
+dummyObj.message = 'Властивість обєкта';
+
+console.log('dummyObj', dummyObj);
+
+console.log(dummyObj.message);
